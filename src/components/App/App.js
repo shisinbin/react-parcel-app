76308ef24a03efeb3b897ab20890ee8ad9306f13
ssb <sb1664@gmail.com>
2023-10-16 12:15:20 +0100

    Replace content with something basic

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,13 +3,8 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>
-        Hello, this be here my first React app made using Parcel.
-      </h1>
-      <p>
-        Not sure of the benefits to doing things this way rather than
-        with create-react-app, but hey, it's all about learning!
-      </p>
+      <h1>Hello, World!</h1>
+      <p>This is an app made using Parcel!</p>
     </div>
   );
 }
