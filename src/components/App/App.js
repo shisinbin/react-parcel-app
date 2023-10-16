0bfd3d3514345b41903de3da0ca68f12dc21c4f6
ssb <sb1664@gmail.com>
2023-10-16 10:55:45 +0100

    Initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Hello, this be here my first React app made using Parcel.
+      </h1>
+      <p>
+        Not sure of the benefits to doing things this way rather than
+        with create-react-app, but hey, it's all about learning!
+      </p>
+    </div>
+  );
+}
+
+export default App;
